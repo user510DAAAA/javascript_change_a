62590814295b63d7fd4b6a8cc123377d44af8f82
user510DAAAA <131327154+user510DAAAA@users.noreply.github.com>
2024-10-08 10:38:47 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// JavaScript
+//console.log('Hello world!');
+
+const btn = document.querySelector('#btn');
+
+btn.addEventListener('click', () => {
+  document.body.classList.toggle('black');
+});
